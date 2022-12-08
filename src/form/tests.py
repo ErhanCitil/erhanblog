0b3af7d4c833e5f73cc7bd2dc27913fcc03a5649
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-08 14:18:45 +0100

    Tests for the contact form, issue with creating db

--- src/form/tests.py
+++ src/form/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
+from .models import *
+from .forms import *
+from .views import *
+from django.urls import reverse
 # Create your tests here.
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.contact = ContactModel.objects.create(
+            name='test', email='test@gmail.com', message='Test message')
+    def test_contact_model(self):
+        self.assertEqual(self.contact.name, self.contact.name)
+        self.assertEqual(self.contact.email, self.contact.email)
+        self.assertEqual(self.contact.message, self.contact.message)
+
+class ContactViewTest(TestCase):
+    def setUp(self):
+        self.contact1 = ContactModel.objects.create(name='erhan', email='erhancitil@test.nl', message='test message3')
+        self.contact2 = ContactModel.objects.create(name='alex', email='alex123@live.nl', message='test message2')
+        self.contact3 = ContactModel.objects.create(name='test', email='test@gmail.nl', message='test message1')
+
+    def test_contact_view(self):
+        url = reverse('contact')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
