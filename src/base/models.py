ba5cd77c250ec59495d7a43edd98022ca0cee30e
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-08 15:01:09 +0100

    Author added to the model & first letter uppercase inside the html file of the author

--- src/base/models.py
+++ src/base/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.title
