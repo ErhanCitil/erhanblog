7c5a426bbfebde2f5c94e8e5fa5119d3f2fdae3b
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-08 16:36:17 +0100

    Fixed path issue

--- src/base/models.py
+++ src/base/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,  null = True, default=1)
     image = models.ImageField(upload_to='blogimages/', blank=True, null=True)
 
     def __str__(self):
--- src/base/tests.py
+++ src/base/tests.py
@@ -3,6 +3,8 @@ from .models import *
 from django.urls import reverse_lazy
 from .views import *
 from django.urls import reverse
+from django.test import Client
+from django.contrib.auth.models import User
 # Create your tests here.
 class PostTest(TestCase):
     def setUp(self):
@@ -20,6 +22,8 @@ class PostTest(TestCase):
 
 class PostTestView(TestCase):
     def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password = '12345')
         self.post1 = Post.objects.create(title='test', content='test content')
         self.post2 = Post.objects.create(title='test2', content='test content2')
         self.post3 = Post.objects.create(title='test3', content='test content3')
@@ -30,7 +34,7 @@ class PostTestView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_post_detail_view(self, **kwargs):
+    def test_post_detail_view(self):
         url = reverse('post', args=[self.post1.pk])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -41,3 +45,13 @@ class PostTestView(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blogs.html')
+
+class ImageFieldTest(TestCase):
+    def test_image_field(self):
+        post = Post.objects.create(title='test', content='test content')
+        self.assertFalse(post.image)
+
+    def test_image_field_upload(self):
+        post = Post.objects.create(title='test', content='test content')
+        post.image = 'test.jpg'
+        self.assertEqual(post.image, 'test.jpg')
