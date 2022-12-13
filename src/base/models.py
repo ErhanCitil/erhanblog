e2c6e6c76542f2efd76698aa8b7da7512786f490
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-13 17:42:33 +0100

    Displaying the author names with a for loop in the template, tomorrow trying with a function

--- src/base/models.py
+++ src/base/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.conf import settings
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=100)
+class Author(models.Model):
+    author_name = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.author_name.username
+
+class Article(models.Model):
+    title = models.CharField(max_length = 1000)
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,  null = True, default=1)
     image = models.ImageField(upload_to='blogimages/', blank=True, null=True)
+    author = models.ManyToManyField(Author, related_name="articles")
 
     def __str__(self):
         return self.title
--- src/base/views.py
+++ src/base/views.py
@@ -6,19 +6,19 @@ from django.urls import reverse_lazy
 class Index(generic.ListView):
     template_name = 'index.html'
     context_object_name = 'posts'
-    model = Post
+    model = Article
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all().order_by('?')[:3]
+        context['posts'] = Article.objects.all().order_by('?')[:3]
         return context
 
 class PostDetail(generic.DetailView):
     template_name = 'blog.html'
-    model = Post
+    model = Article
     context_object_name = 'post'
 
 class Blogs(generic.ListView):
     template_name = 'blogs.html'
     context_object_name = 'posts'
-    model = Post
+    model = Article
