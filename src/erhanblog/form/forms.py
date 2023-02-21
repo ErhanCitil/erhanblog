b1674a158a16ba045e54469506ccea2469485b63
Erhan <erhan.citil@maykinmedia.nl>
2023-02-21 12:19:23 +0100

    💄 Added form control of Bootstrap to the contact form

--- src/erhanblog/form/forms.py
+++ src/erhanblog/form/forms.py
@@ -5,4 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactModel
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+        }
         
